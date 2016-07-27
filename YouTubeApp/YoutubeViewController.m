e4c77e6b227271e17b6a4ed876c0714e3d9a8c15
AnthonyMa0101 <[anthony.ma111@gmail.com]>
2016-07-27 13:14:14 -0400

    generated values in array

--- YouTubeApp/YoutubeViewController.m
+++ YouTubeApp/YoutubeViewController.m
@@ -7,11 +7,22 @@
 //
 
 #import "YoutubeViewController.h"
+#import "ViewController.h"  
+#import "AppDelegate.h"
 #import "YTPlayerView.h"
+#import "Video.h"
 
 @interface YoutubeViewController () <YTPlayerViewDelegate>
 
+@property (nonatomic, strong) NSMutableDictionary *videoDictionary;
+
+@property (nonatomic, strong) NSMutableArray *commentsArray;
+
 @property (nonatomic, strong) NSMutableArray *videoArray;
+
+@property (nonatomic, strong) Video *video;
+
+@property (nonatomic, strong) AppDelegate *appDelegate;
 
 @end
 
@@ -20,13 +31,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.playerView.delegate = self;
+//    AppDelegate *appDelegate = [[AppDelegate alloc] init];
     
+//    [appDelegate.managedObjectContext executeRequest:appDelegate.persistentStoreCoordinator error:(NSError * _Nullable __autoreleasing * _Nullable)];
+    
+    //set player view parameters
+    self.playerView.delegate = self;
     self.playerView.backgroundColor = [UIColor blackColor];
     
-    [self apiFetch];
+    //api request
+//    [self apiFetch];
+    [self apiCommentsFetch];
     
-//    [self.playerView loadVideoById:@"UCE_M8A5yxnLfW0KghEeajjw" startSeconds:1 suggestedQuality:4];
+    //set and load video to player
+    self.video = [self createVideo];
+    [self videoID:self.video andID:@""];
+    [self.playerView loadWithVideoId:@"PQ4y2MNvJK0"];
     
     // Do any additional setup after loading the view.
 }
@@ -46,21 +66,56 @@
 }
 */
 
-#pragma mark - API Initiation
+#pragma mark - AppDelegate Properties Access
 
-- (NSURL *)URL {
-    return [NSURL URLWithString:@"https://www.googleapis.com/youtube/v3/channels?part=contentDetails,snippet&forUsername=Apple&key=AIzaSyCK8NV2bi5TPJ3-wa60C5vEqQMGEx8CQP4"];
+
+
+#pragma mark - Video Initiation 
+
+- (Video *)createVideo {
+    Video *video = [[Video alloc] init];
+    return video;
 }
 
+- (void)videoID:(Video *)video andID:(NSString *)videoID {
+    video.videoID = videoID;
+}
+
+#pragma mark - API Initiation
+
+//- (NSURL *)URLforComment {
+//    NSString *commentAPI = @"https://www.googleapis.com/youtube/v3/commentThread?
+//}
+
+- (NSURL *)URLforCommentThread {
+    NSString *apiKey = @"AIzaSyCK8NV2bi5TPJ3-wa60C5vEqQMGEx8CQP4";
+    NSString *urlOne = @"https://www.googleapis.com/youtube/v3/commentThread?";
+    NSString *part = @"part=videoID&textFormat&key=";
+    NSString *appended = [NSString stringWithFormat:@"%@%@%@", urlOne, part, apiKey];
+    NSURL *URL = [NSURL URLWithString:appended];
+    return URL;
+}
+
+- (NSURL *)URLforVideo {
+    NSString *apiKey = @"AIzaSyCK8NV2bi5TPJ3-wa60C5vEqQMGEx8CQP4";
+    NSString *urlOne = @"https://www.googleapis.com/youtube/v3/videos?";
+    NSString *part = @"part=contentDetails&chart=mostPopular&key=";
+    NSString *appended = [NSString stringWithFormat:@"%@%@%@", urlOne, part, apiKey];
+    NSURL *URL = [NSURL URLWithString:appended];
+    return URL;
+}
+ 
 #pragma mark - API Request
 
-- (void)apiFetch {
-
-NSURLRequest *urlRequest = [NSURLRequest requestWithURL:[self URL]];
-NSURLSession *sharedSession = [NSURLSession sharedSession];
-
-NSURLSessionDataTask *dataTask = [sharedSession dataTaskWithRequest:urlRequest completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+- (void)apiCommentsFetch {
     
+    NSString *urlString = @"https://www.googleapis.com/youtube/v3/search?&part=snippet&q=happy&type=video&videoEmbeddable=true&order=relevance&key=AIzaSyCK8NV2bi5TPJ3-wa60C5vEqQMGEx8CQP4&pageToken=CAUQAA";
+    NSURL *url = [NSURL URLWithString:urlString];
+
+    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
+    NSURLSession *sharedSession = [NSURLSession sharedSession];
+
+    NSURLSessionDataTask *dataTask = [sharedSession dataTaskWithRequest:urlRequest completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
     
     NSLog(@"Request Done");
     
@@ -70,23 +125,36 @@ NSURLSessionDataTask *dataTask = [sharedSession dataTaskWithRequest:urlRequest c
         
         NSDictionary *videosListing = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
         
+        NSMutableArray *tempArray = [[NSMutableArray alloc] init];
+        
         if (!jsonError) {
             
-            NSLog(@"Data: %@", data);
+//            NSLog(@"Data: %@", data);
+            [self logJSONData:videosListing];
             
-            NSLog(@"JSON: %@", videosListing);
-            
+            for (NSDictionary *properties in videosListing[@"items"]) {
+                if ([properties isKindOfClass:[NSDictionary class]]) {
+                    Video *video = [[Video alloc] init];
+                    NSString *imageString = properties[@"snippet"][@"thumbnails"][@"default"][@"url"];
+                    NSURL *urlImage = [NSURL URLWithString:imageString];
+                    NSData *imageData = [NSData dataWithContentsOfURL:urlImage];
+                    video.videoThumbnail = [UIImage imageWithData:imageData];
+                    video.videoID = properties[@"id"][@"videoId"];
+                    [tempArray addObject:video];
+                }
+            }
         }
         else {
-                    NSLog(@"NO");
+            NSLog(@"NO");
         }
-            
+        
+        self.videoArray = tempArray;
+        
             dispatch_async(dispatch_get_main_queue(), ^{
                 self.title = [NSString stringWithFormat:@"Your Moody Videos"];
 //                [self.collectionView reloadData];
             });
     }
-    
     else {
         NSLog(@"Request error: %@", error.localizedDescription);
     }
@@ -97,6 +165,23 @@ NSURLSessionDataTask *dataTask = [sharedSession dataTaskWithRequest:urlRequest c
 
 }
 
+- (NSString *)searchParameters {
+    return @"";
+}
 
+- (void)searchString {
+    ViewController *viewController = [[ViewController alloc] init];
+    NSString *searchParameters = @"Joyful, ";
+}
+
+- (void)logJSONData:(NSDictionary *)list {
+    NSLog(@"JSON: %@", list);
+}
+
+#pragma mark - API results 
+
+- (void)videoIDs {
+    
+}
 
 @end
